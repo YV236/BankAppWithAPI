32791521fc72880c0d85f0b3b859cbe10f0dda46
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-06-25 16:17:01 +0300

    UserUpdate

--- BankAppWithAPI/Models/User.cs
+++ BankAppWithAPI/Models/User.cs
@@ -4,7 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string UserName { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
