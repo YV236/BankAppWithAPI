397aed0a097ab4f983184e63c3fe7ef927042d6c
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-06-25 16:19:43 +0300

    UserDtosUpdate

--- BankAppWithAPI/Dtos/User/UpdateUserDto.cs
+++ BankAppWithAPI/Dtos/User/UpdateUserDto.cs
@@ -3,6 +3,7 @@
     public class UpdateUserDto
     {
         public string UserName { get; set; } = string.Empty;
+        public string UserSurname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
--- BankAppWithAPI/Dtos/User/UserRegisterDto.cs
+++ BankAppWithAPI/Dtos/User/UserRegisterDto.cs
@@ -2,7 +2,8 @@
 {
     public class UserRegisterDto
     {
-        public string UserName { get; set; } = string.Empty;
+        public string UserName { get; set; } 
+        public string UserSurname { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
