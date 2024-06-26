a228a6b7e29dda89a11f16e513478531dd09b458
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-06-26 18:37:45 +0300

    MigrationExtension

---
+++ BankAppWithAPI/Extensions/MigrationExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAppWithAPI.Extensions
+{
+    public static class MigrationExtension
+    {
+        public static void ApplyMigration(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using DataContext context = scope.ServiceProvider.GetService<DataContext>();
+            context.Database.Migrate();
+        } 
+    }
+}
