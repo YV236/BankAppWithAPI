c312fe50e330e9df59715b959d38a43ae48663bd
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-10-28 18:24:36 +0200

    Add OperationRequestDto mapping for specific operation classes

--- BankAppWithAPI/AutoMapperProfile.cs
+++ BankAppWithAPI/AutoMapperProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using BankAppWithAPI.Dtos.BankAccount;
 using BankAppWithAPI.Dtos.Card;
+using BankAppWithAPI.Dtos.Operation;
 using BankAppWithAPI.Dtos.User;
 using BankAppWithAPI.Models;
+using BankAppWithAPI.Models.Operations;
 using Microsoft.AspNetCore.Identity;
 
 namespace BankAppWithAPI
@@ -17,6 +19,9 @@ namespace BankAppWithAPI
             CreateMap<UpdateUserDto, User>();
             CreateMap<Card, GetCardDto>();
             CreateMap<BankAccount, GetBankAccountDto>();
+            CreateMap<OperationRequestDto, DepositOperation>();
+            CreateMap<OperationRequestDto, WithdrawOperation>();
+            CreateMap<OperationRequestDto, TransferOperation>();
         }
     }
 }
