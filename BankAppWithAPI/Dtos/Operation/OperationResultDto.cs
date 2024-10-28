9058136ad8eb1468e82f4a490b5ffb0f4835d0f3
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-10-28 15:48:32 +0200

    OperationResultDto class creation

--- BankAppWithAPI/Dtos/Operation/OperationResultDto.cs
+++ BankAppWithAPI/Dtos/Operation/OperationResultDto.cs
@@ -1,6 +1,14 @@
-﻿namespace BankAppWithAPI.Dtos.Operation
+﻿using BankAppWithAPI.Models.Operations;
+
+namespace BankAppWithAPI.Dtos.Operation
 {
     public class OperationResultDto
     {
+        public string IBAN { get; set; } = string.Empty;
+        public string AccountName { get; set; } = string.Empty;
+        public decimal Amount { get; set; } = decimal.Zero;
+        public decimal BalanceAfter { get; set; } = decimal.Zero;
+        public DateTime? OperationDate { get; set; }
+        public OperationType? OperationType { get; set; }
     }
 }
