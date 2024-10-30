4f166db9a1de8dff9899a3a6eb93117cc680e05f
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-10-30 16:08:46 +0200

    OperationRequestDto update

--- BankAppWithAPI/Dtos/Operation/OperationRequestDto.cs
+++ BankAppWithAPI/Dtos/Operation/OperationRequestDto.cs
@@ -4,7 +4,6 @@ namespace BankAppWithAPI.Dtos.Operation
 {
     public class OperationRequestDto
     {
-        public string IBAN { get; set; } = string.Empty;
         public string DestinationIBAN { get; set; } = string.Empty;
         public decimal Amount { get; set; } = 0;
         public OperationType OperationType { get; set; }
