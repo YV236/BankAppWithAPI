350ebc9ae76e7a8779b38056ee93aaf7002b0573
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-10-30 16:09:22 +0200

    CardExtension class

---
+++ BankAppWithAPI/Extensions/CardExtension.cs
@@ -0,0 +1,15 @@
+﻿using BankAppWithAPI.Data;
+using BankAppWithAPI.Models;
+
+namespace BankAppWithAPI.Extensions
+{
+    public static class CardExtension
+    {
+        public static async Task<BankAccount> FindActiveAccount (this string cardNumber, DataContext _context)
+        {
+            var card = await _context.Cards.Include(c => c.AccountCards).FirstOrDefaultAsync(c => c.CardNumber == cardNumber);
+
+            return card!.AccountCards.FirstOrDefault(u => u.Account!.IsActive == true)!.Account!;
+        }
+    }
+}
