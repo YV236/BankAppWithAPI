237d75911ceb4a4a5b48fd08b49d672408e75e49
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-11-01 17:20:08 +0200

    CreateToken method

--- BankAppWithAPI/Extensions/HashingExtension.cs
+++ BankAppWithAPI/Extensions/HashingExtension.cs
@@ -1,7 +1,7 @@
-﻿
-
-using BankAppWithAPI.Models;
+﻿using BankAppWithAPI.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace BankAppWithAPI.Extensions
 {
@@ -18,14 +18,44 @@ namespace BankAppWithAPI.Extensions
                 }
             }
         }
-
         public static bool VerifyPasswordHash(string pinCode, byte[] pinHash, byte[] pinSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(pinSalt))
+            using (var pbkdf2 = new System.Security.Cryptography.Rfc2898DeriveBytes(pinCode, pinSalt, 10000))
             {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(pinCode));
+                var computedHash = pbkdf2.GetBytes(32);
                 return computedHash.SequenceEqual(pinHash);
             }
+        }
+
+        public static string? CreateToken(Card card)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, card.Id.ToString())
+            };
+            var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+
+            if(secretKey is null)
+            {
+                throw new Exception("Key token is null");
+            }
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+               .GetBytes(secretKey));
+
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
