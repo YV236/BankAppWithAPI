86f82d0e8d111febf0aa15fc5b217f5808de97b4
Yurii Vinnyk <yuravinnyk67@gmail.com>
2024-11-01 14:05:28 +0200

    LoginCardDto creation

---
+++ BankAppWithAPI/Dtos/Card/LoginCardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BankAppWithAPI.Dtos.Card
+{
+    public class LoginCardDto
+    {
+        public string CardNumber { get; set; } = string.Empty;
+        public short PinCode { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
